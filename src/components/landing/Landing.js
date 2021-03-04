1612e7aad16ada14abaad3311c9856091babd9f2
IronHarambe <49575350+IronHarambe@users.noreply.github.com>
2021-03-04 14:35:50 +1100

    home page updated with text and button

--- src/components/landing/Landing.js
+++ src/components/landing/Landing.js
@@ -1,11 +1,30 @@
 import React from "react";
+import Button from "@material-ui/core/Button";
 
 import "./Landing.scss";
 
+/* Lannding page containes full page image and highlight text */
+
 function Landing() {
   return (
-    <div className="landing">
-      <h1>Samson Jose</h1>
+    <div className="landing" id="#home">
+      <div className="landing__text">
+        <h1>
+          Hi<span className="comma">,</span> I'm
+          <span className="name"> Samson Jose</span>
+        </h1>
+        <h6>I'm a beginner web & software dev.</h6>
+        <div className="project__btn">
+          <Button
+            variant="outlined"
+            color="primary"
+            href="#Projects"
+            className="btn"
+          >
+            Projects
+          </Button>
+        </div>
+      </div>
     </div>
   );
 }
