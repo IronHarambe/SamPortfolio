8b4308e26f02d3c36125e0932fe8413e65b5f341
IronHarambe <49575350+IronHarambe@users.noreply.github.com>
2021-03-09 13:56:41 +1100

    Icon stacks added and footer mobile view done

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,7 +1,4 @@
 import React from "react";
-import GitHubIcon from "@material-ui/icons/GitHub";
-import LinkedInIcon from "@material-ui/icons/LinkedIn";
-import PictureAsPdfIcon from "@material-ui/icons/PictureAsPdf";
 
 import "./Footer.scss";
 import IconStack from "../icon-stack/IconStack";
